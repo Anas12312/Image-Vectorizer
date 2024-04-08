cdce6216052ba6004f621071a11240b2d33eba1c
anas12312 <anashesham000@gmail.com>
2024-04-08 19:42:05 +0200

    anas

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+
+
+  return (
+    <div className='w-screen h-screen flex flex-col justify-center items-center'>
+      <div className="font-bold text-5xl text-black">Trace Pixels To Vectors in Full Color</div>
+      <div className="text-center text-lg text-gray-400 mt-8">Convert your PNG and JPG images to SVG vectors quickly and easily. <br /> Fully automatically. Using AI.</div>
+      <div className="w-[50%] h-[30%] border-2 border-blue-500 text-blue-500 border-dashed rounded-lg mt-8 flex justify-center items-center font-bold text-3xl italic">
+        UPLOAD YOUR IMAGE.
+      </div>
+    </div>
+  )
+}
+
+export default App
